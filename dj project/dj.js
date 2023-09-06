66eba568c73a3fc93773d442522fa984fa0fdd6f
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2023-09-06 19:24:43 -0400

    initial commit

---
+++ dj project/dj.js
@@ -0,0 +1,12 @@
+var intBox = document.getElementById("the-box");
+
+
+function changeColor (color) {
+    intBox.style.backgroundColor = color;
+}
+
+intBox.addEventListener("mouseover", function(){changeColor ("blue")} )
+intBox.addEventListener("mousedown", function(){changeColor ("red")} )
+intBox.addEventListener("mouseup", function(){changeColor ("yellow")} )
+intBox.addEventListener("dblclick", function(){changeColor ("green")} )
+intBox.addEventListener("scroll", function(){changeColor ("orange")} )
