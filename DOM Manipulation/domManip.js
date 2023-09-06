41cf6b2f91ee2df2575976b687d16dd971adeef2
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2023-09-06 19:25:07 -0400

    initial commit

--- DOM Manipulation/domManip.js
+++ DOM Manipulation/domManip.js
@@ -1,3 +1,8 @@
 var newHeading = document.getElementById("new_heading");
+let divTarget = document.getElementById("Main_Box");
 
 newHeading.innerHTML = "Did this work?";
+let newButton = document.createElement("button");
+newButton.innerHTML = "New Button!";
+newButton.setAttribute("id","new-button");
+divTarget.append(newButton);
