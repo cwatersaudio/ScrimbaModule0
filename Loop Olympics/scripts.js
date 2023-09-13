7cfc652d6b3274b763d822fa0d1cc371df03cd5c
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2023-09-13 11:17:42 -0400

    completed assignments

---
+++ Loop Olympics/scripts.js
@@ -0,0 +1,12 @@
+let arr = [];
+let bigArr =[];
+
+for ( j = 0; j < 3; j++) {
+for ( i = 0; i < 3; i++) {    
+ for (let i = 0; i<3; i++) {
+        arr[i] = "0";
+      }
+      bigArr[i] = arr;
+    }
+console.log(bigArr[j])
+}  
